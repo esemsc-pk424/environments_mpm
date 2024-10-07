a6a3f9cc60286b276d5b7ce59d0ae8ddc91d8a87
acse-pk424 <pk424@ic.ac.uk>
2024-10-07 17:02:58 +0100

    Second commit, added gen_table

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'gen_table']
 
 def rand_array(shape):
     return np.random.rand(*shape)
@@ -12,3 +13,6 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def gen_table(n):
+    return print(pd.DataFrame([0,0,0,0,0], index=np.arange(n)))
---
+++ scripts/gen_table.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.append('..')
+
+from envtest import gen_table
+
+gen_table(5)
